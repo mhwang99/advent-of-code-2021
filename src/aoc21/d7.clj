84d97e44be60d0f8340bb8883bdb4973220185c3
Mark Hwang <mark.hwang@owl.co>
2021-12-07 02:32:53 -0500

    day 7

---
+++ src/aoc21/d7.clj
@@ -0,0 +1,34 @@
+(ns aoc21.d7
+  (:require [clojure.string :as s]
+            [aoc21.core :as aoc :refer :all]))
+
+(defn diff [a b] (if (< a b) (- b a) (- a b)))
+
+(defn q0
+  [fn-fuel l]
+  (let [l (sort l)]
+    (loop [i (first l)
+           min-fuel nil]
+      (if (> i (last l))
+        min-fuel
+        (let [fuel (reduce #(-> (diff %2 i) fn-fuel (+ %1)) 0 l)]
+          (if (or (nil? min-fuel) (<= fuel min-fuel))
+            (recur (inc i) fuel)
+            min-fuel))))))
+
+(def sigma
+ (memoize
+  (fn [n]
+    (if (zero? n) 0
+      (+ (sigma (dec n)) n)))))
+
+(def q1 (partial q0 identity))
+(def q2 (partial q0 sigma))
+
+(def in (get-num))
+(def in2 [16,1,2,0,4,2,7,1,2,14])
+
+#_(q1 in2)
+#_(q2 in2)
+#_(q1 in)
+#_(q2 in)
