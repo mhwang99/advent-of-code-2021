579abc1abbf5795192afbc4ec2aa039924a7c002
Mark Hwang <mark.hwang@owl.co>
2021-12-18 16:32:04 -0500

    day 18

--- src/aoc21/d18.clj
+++ src/aoc21/d18.clj
@@ -2,27 +2,6 @@
   (:require [aoc21.core :as aoc :refer :all]
             [clojure.string :as s]))
 
-(defn splits?
-  ([fish] (splits? fish []))
-  ([fish ks]
-   (when-let [v (get-in fish ks)]
-     (if (number? v)
-       (when (> v 9)
-         [ks v])
-       (or (splits? fish (conj ks 0))
-           (splits? fish (conj ks 1)))))))
-
-(defn explode?
-  ([fish] (explode? fish []))
-  ([fish ks]
-   (let [v (get-in fish ks)]
-     (when (vector? v)
-       (let [[lv rv] v]
-         (if (and (number? lv) (number? rv) (>= (count ks) 4))
-           [ks lv rv]
-           (or (explode? fish (conj ks 0))
-               (explode? fish (conj ks 1)))))))))
-
 (defn get-near [d fish ks]
   (loop [me (peek ks)
          ks (pop ks)]
@@ -33,25 +12,43 @@
                      (recur (conj ks d)))))
       (seq ks) (recur (peek ks) (pop ks))
       :else nil)))
-
 (def get-left (partial get-near 1))
 (def get-right (partial get-near 0))
 
+(defn search-splits
+  ([fish] (search-splits fish []))
+  ([fish ks]
+   (when-let [v (get-in fish ks)]
+     (if (number? v)
+       (when (> v 9)
+         [ks v])
+       (or (search-splits fish (conj ks 0))
+           (search-splits fish (conj ks 1)))))))
+
 (defn splits [fish]
-  (when-let [[ks v] (splits? fish)]
+  (when-let [[ks v] (search-splits fish)]
     (let [lv (quot v 2)
           rv (- v lv)]
       (assoc-in fish ks [lv rv]))))
 
+(defn search-explode
+  ([fish] (search-explode fish []))
+  ([fish ks]
+   (let [v (get-in fish ks)]
+     (when (vector? v)
+       (let [[lv rv] v]
+         (if (and (number? lv) (number? rv) (>= (count ks) 4))
+           [ks lv rv]
+           (or (search-explode fish (conj ks 0))
+               (search-explode fish (conj ks 1)))))))))
+
 (defn explode [fish]
-  (when-let [[ks lv rv] (explode? fish)]
+  (when-let [[ks lv rv] (search-explode fish)]
     (let [[lks llv] (get-left fish ks)
-          [rks rrv] (get-right fish ks)
-          llv (if lks (+ llv lv) 0)
-          rrv (if rks (+ rrv rv) 0)]
+          [rks rrv] (get-right fish ks)]
       (cond-> (assoc-in fish ks 0)
-        lks (assoc-in lks llv)
-        rks (assoc-in rks rrv)))))
+        lks (assoc-in lks (+ llv lv))
+        rks (assoc-in rks (+ rrv rv))))))
 
 (defn magnitude
   ([v] (magnitude v 1))
